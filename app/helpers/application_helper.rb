a72524565cb740e9d2188aafcca7e64b7d6db7ba
Becky Osiecki <lobeliaushtar@gmail.com>
2014-03-20 14:36:25 -0400

    Rails II - ActionSpecificLayoutContent - complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
+	def title(title)
+		content_for(:title, title)
+	end
 
+	def page_title
+		if content_for?(:title)
+			content_tag(:title, "Flix - #{content_for(:title)}")
+		else
+			content_tag(:title, "Flix")
+		end
+	end
 end
